f4a71fd05aebdc0cefeb1b0ebdf85420b5361239
mdshafeek <50830072+mdshafeek@users.noreply.github.com>
2020-02-10 22:26:00 -0800

    Add files via upload
    simple calculator program

---
+++ py2.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+sum = float(num1) + float(num2)
+sub = float(num1) + float(num2)
+mul = float(num1) * float(num2)
+div = float(num1) / float(num2)
+rem = float(num1) % float(num2)
+print("sum: "+str(sum)+"\nDifference: "+str(sub)+"\nProduct: "+str(mul)+"\nQuotient: "+str(div)+"\nRemainder: "+str(rem))
